896c775495526a8b3adc4fe92ee259d2ac91d103
morishita1991 <vw2gmldqbelief@gmail.com>
2023-07-11 11:42:00 +0900

    チケット枚数の登録テストケースをREADMEに記載

--- test/ValidateQuantityInputTest.php
+++ test/ValidateQuantityInputTest.php
@@ -69,9 +69,24 @@ class ValidateQuantityInputTest extends TestCase
 
     /**
      * チケット枚数の入力値バリデーション
-     * --- 失敗例: 入力値「'1001'」
+     * --- 失敗例: 入力値「'-1'」
      */
     public function testValidateQuantityInput_Error02()
+    {
+        $input = '-1';
+        $_SESSION['unittest_ask_input'] = $input;
+        $quantity = new Quantity();
+        $quantity->validate();
+        $this->assertFalse($quantity->validResult['result']);
+        $this->assertSame('1から1000までの半角数字で入力してください。', $quantity->validResult['error']);
+        unset($_SESSION['unittest_ask_input']);
+    }
+
+    /**
+     * チケット枚数の入力値バリデーション
+     * --- 失敗例: 入力値「'1001'」
+     */
+    public function testValidateQuantityInput_Error03()
     {
         $input = '1001';
         $_SESSION['unittest_ask_input'] = $input;
@@ -79,6 +94,36 @@ class ValidateQuantityInputTest extends TestCase
         $quantity->validate();
         $this->assertFalse($quantity->validResult['result']);
         $this->assertSame('1から1000までの半角数字で入力してください。', $quantity->validResult['error']);
+        unset($_SESSION['unittest_ask_input']);
+    }
+
+    /**
+     * チケット枚数の入力値バリデーション
+     * --- 失敗例: 入力値「'１'」
+     */
+    public function testValidateQuantityInput_Error04()
+    {
+        $input = '１';
+        $_SESSION['unittest_ask_input'] = $input;
+        $quantity = new Quantity();
+        $quantity->validate();
+        $this->assertFalse($quantity->validResult['result']);
+        $this->assertSame('半角数字で入力してください。', $quantity->validResult['error']);
+        unset($_SESSION['unittest_ask_input']);
+    }
+
+    /**
+     * チケット枚数の入力値バリデーション
+     * --- 失敗例: 入力値「'いち'」
+     */
+    public function testValidateQuantityInput_Error05()
+    {
+        $input = 'いち';
+        $_SESSION['unittest_ask_input'] = $input;
+        $quantity = new Quantity();
+        $quantity->validate();
+        $this->assertFalse($quantity->validResult['result']);
+        $this->assertSame('半角数字で入力してください。', $quantity->validResult['error']);
         unset($_SESSION['unittest_ask_input']);
     }
 }
